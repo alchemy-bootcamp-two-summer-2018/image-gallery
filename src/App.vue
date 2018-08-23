7c6d58240c9cc175fd7c06ce753423422cfc8a9c
Dyanna Tolman <dyannast@gmail.com>
2018-08-23 15:55:49 -0700

    linked router to about and home

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div id="app">
+  <div class="app">
+    <header>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </header>
+
+  <router-view></router-view>
+    
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<main>
+<h1>Home Page</h1>
+<router-link :to="`/home/$`">
+
+</router-link>
+</main>
+    
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
