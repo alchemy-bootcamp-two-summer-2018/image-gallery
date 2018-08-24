30442de50d4f3d91cf7b1d31fb704b11a366c3b3
laurenshareshian <laurenshareshian@gmail.com>
2018-08-24 16:02:53 -0700

    single gallery image display

---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="thumbnail">
+        <img :src="image.url" :alt="image.id" />
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    image: Object
+  },
+};
+</script>
+
+<style scoped>
+
+
+</style>
--- src/components/GalleryViewer.vue
+++ src/components/GalleryViewer.vue
@@ -1,17 +1,56 @@
 <template>
-  <div>
-    Gallery Page
-  </div>
-
+    <div class="gallery">
+        <GalleryItem v-if="image"
+        :key="image.id"
+        :image="image"
+        /> 
+    </div>
 </template>
 
 <script>
+import GalleryItem from './GalleryItem';
 
 export default {
+  props: {
+    album: Object
+  },
+  components: {
+    GalleryItem
+  },
+  data() {
+    return {
+      image: this.album.images[0]
+    };
+  },
 };
-
 </script>
 
-<style>
+<style scoped>
+
+/* ul.tile-view {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 5px;
+}
+
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 5px;
+  padding-left: 0;
+}
+
+li {
+  list-style: none;
+  text-align: center;
+  border: 1px solid #aaa;
+  padding-top: 60px;
+}
+
+a {
+  text-decoration: none;
+  color: initial;
+} */
+
 
 </style>
