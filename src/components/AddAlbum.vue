bf5a9983f4f291dc34b455e5b681158b4593e5d9
Claire Flanagan <claireflanagan@gmail.com>
2018-08-24 13:40:46 -0700

    hooking up data.js

--- src/components/AddAlbum.vue
+++ src/components/AddAlbum.vue
@@ -1,14 +1,46 @@
 <template>
-  <main>
+  <form v-on:submit.prevent="handleSubmit">
     <h1>Add an Album!</h1>
-
-  </main>
-    
+    <p>
+      <label>
+        Album Title: <input v-model="title" placeholder="Title">
+      </label>
+    </p>
+    <p>
+      <label>
+        Description: <input v-model="description" placeholder="Description">
+      </label>
+    </p>
+    <p>
+      <button>Add Album</button>
+    </p>
+  </form>
 </template>
 
 <script>
 export default {  
+  props: {
+    onAdd: Function
+  },
+  data() {
+    return {
+      title: '',
+      description: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const newAlbum = {
+        title: this.title,
+        description: this.description
+      };
+      this.onAdd(newAlbum);
 
+      this.title = '';
+      this.description = '';
+
+    }
+  }
 };
 </script>
 
