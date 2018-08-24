05633e76c46c88c57ed2cd887223907f874eb7cb
kkrosefields <kkrosefields@gmail.com>
2018-08-24 13:12:03 -0700

    added list view page

---
+++ src/components/ListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list-item">
+   <h3>{{ album.title }}</h3>
+      <p>Description: {{ album.description }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    image: Object
+  }
+};
+
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  text-align: center;
+  border: 1px solid #aaa;
+}
+img {
+  height: 200px;
+}
+</style>
--- src/components/ListViewer.vue
+++ src/components/ListViewer.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    List Page
-  </div>
+  <li class="">
+        <ImageItem v-if="album"
+        v-for="image in album.images"
+        :key="image.url"
+        :image="image"
+        /> 
+  </li>
 
 </template>
 
 <script>
+import ListItem from './ListItem';
 
 export default {
+  props: {
+    album: Object
+  },
+  components: {
+   ListItem
+  }
 };
-
 </script>
 
 <style>
