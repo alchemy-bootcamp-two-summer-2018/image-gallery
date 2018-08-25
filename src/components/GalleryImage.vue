baaf951c5489dd2dcb9fab0d6fc2ba7ddb53ce66
Claire Flanagan <claireflanagan@gmail.com>
2018-08-24 17:43:11 -0700

    gallery working

---
+++ src/components/GalleryImage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>{{ image.title }}</h3>
+        <img :src="image.url">
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: Object
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/components/GalleryViewer.vue
+++ src/components/GalleryViewer.vue
@@ -1,13 +1,40 @@
 <template>
     <main>
         <h1>Gallery!</h1>
+        <GalleryImage v-if="image"
+          :key="image.id"
+          :image="image"
+          />
+        <button v-on:click="handleClick">Next</button>
     </main>
-    
 </template>
 
 <script>
-export default {
+import GalleryImage from './GalleryImage.vue';
 
+export default {
+  props: {
+    album: Object
+  },
+  components: {
+    GalleryImage
+  },
+  data() {
+    return {
+      image: this.album.images[0],
+      currentImage: 0
+    };
+  },
+  methods: {
+    handleClick: function(){
+      this.currentImage += 1;
+
+      if(this.currentImage === this.album.images.length) {
+        this.currentImage = 0;
+      }
+      this.image = this.album.images[this.currentImage];
+    }
+  }
 };
 </script>
 
