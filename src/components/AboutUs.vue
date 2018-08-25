4bd37bf109ff84bcb161ef907732b335607badf0
laurenshareshian <laurenshareshian@gmail.com>
2018-08-24 17:20:15 -0700

    current version

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="about-bg">
         <div class="text-bg">
-        We are Kayla and Lauren and we like photos.
+        <p>We are Kayla and Lauren and we like photos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
 </template>
@@ -13,13 +13,17 @@ export default {};
 
 <style scoped>
 .text-bg {
-  background:white;
+  width: 700px;
+  padding: 30px;
+  background: white;
+  border-radius: 10px;
 }
 #about-bg {
   background-image: url("/camera.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  padding: 60px;
 }
 
 </style>
