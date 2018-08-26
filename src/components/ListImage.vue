d527539983ec5344f8a1ede6ecdcf132ff216b39
Dyanna Tolman <dyannast@gmail.com>
2018-08-25 23:26:35 -0700

    new ListImage file

---
+++ src/components/ListImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+        <h2>{{ album.title }}</h2>
+        <img :src="image.url"/>
+        <p>{{ image.description }}</p>
+    </li>
+</template>
+
+<script>
+export default {
+  props: {
+    image: Object
+  },
+
+};
+</script>
+
+<style>
+
+</style>
--- src/components/ListViewer.vue
+++ src/components/ListViewer.vue
@@ -1,21 +1,24 @@
 <template>
     <ul class="list-view">
-          <Album v-for="image in album.image "
+          <ListImage v-for="image in album.image"
           :key="image.id"
           :src="image.url"
           />
     </ul>
+
 </template>
 
 <script>
 import Album from './Album';
+import ListImage from './ListImage';
 
 export default {
   props: {
     album: Object
   },
   components: {
-    Album
+    Album,
+    ListImage
   }
 };
 </script>
